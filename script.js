d1761716351315c4d69452e346e7e01e40d99e5b
panasewicz <145676373+panasewicz@users.noreply.github.com>
2023-10-02 13:54:23 +0200

    Update stop-1/4 page script.js

--- script.js
+++ script.js
@@ -4,29 +4,52 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const columns = canvas.width / 10; // szerokość każdej kolumny
-const drops = []; // przechowuje obecną pozycję spadania każdej kolumny
+const word = "PANASEWICZ";
+let letters = Array.from(word);
+const drops = Array(word.length).fill(-1);
+const dropSpeeds = Array(word.length).fill(0);
+let displayTime = 0;
 
-for (let i = 0; i < columns; i++) {
-    drops[i] = 1;
-}
+ctx.font = '26px "Courier New bold"';
 
 function drawMatrix() {
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.05)'; // tło
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = '#00FF00'; // kolor liter
-    ctx.font = '10px Courier New';
+    ctx.fillStyle = '#00FF00';
 
-    for (let i = 0; i < drops.length; i++) {
-        const text = 'PANASEWICZ'[Math.floor(Math.random() * 'PANASEWICZ'.length)]; // wybierz losową literę
-        ctx.fillText(text, i * 10, drops[i] * 10); // rysuj literę
-
-        if (drops[i] * 10 > canvas.height || Math.random() > 0.975) {
-            drops[i] = 0;
+    for (let i = 0; i < letters.length; i++) {
+        if (drops[i] < (canvas.height / 4) && drops[i] >= 0) {
+            const text = letters[i];
+            ctx.fillText(text, i * 26, drops[i]);
+            drops[i] += dropSpeeds[i];
+        } else if (drops[i] >= (canvas.height / 4) && drops[i] !== -1) {
+            ctx.fillText(letters[i], i * 26, canvas.height / 4);
         }
-        drops[i]++;
+    }
+
+    // Start next letter drop
+    for (let i = 0; i < letters.length; i++) {
+        if (drops[i] === -1 && (i === 0 || drops[i - 1] > 75)) {
+            drops[i] = 0;
+            dropSpeeds[i] = Math.random() * 5 + 5; 
+            break;
+        }
+    }
+
+    // Check if all letters are at 1/4 of the screen
+    if (drops.every(d => d >= canvas.height / 4 || d === -1)) {
+        displayTime++;
+
+        if (displayTime >= 60) {
+            // Reset for next cycle after 3 seconds
+            for (let i = 0; i < letters.length; i++) {
+                drops[i] = -1;
+                dropSpeeds[i] = 0;
+            }
+            displayTime = 0;
+        }
     }
 }
 
-setInterval(drawMatrix, 33); // aktualizuj co 33 ms
+setInterval(drawMatrix, 50);  // 20 frames per second
